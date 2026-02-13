9f9402629641aabad1a1744e20b239950db3e53c
991727914 <pate5339@sheridancollege.ca>
2026-02-12 22:59:03 -0500

    My first or initial Commit

---
+++ src/main/java/com/mycompany/projecthinal/ProjectHinal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecthinal;
+
+/**
+ *
+ * @author praxi
+ */
+public class ProjectHinal {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World my first set up");
+    }
+}
+
+
